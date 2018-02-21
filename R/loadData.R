6d1735f419100cc81d05ec6c743eb8b3bc6d0129
Matt Motoki <mhmotoki@gmail.com>
2018-02-21 14:48:52 +0000

    remove hardcoded data path

--- R/loadData.R
+++ R/loadData.R
@@ -1,66 +1,66 @@
-#' Convenience function for loading data. 
-#' 
-#' The function recusively looks for the desired file in 
-#' \code{"C:/Dropbox/Kaggle/Instacart/data"}.  
-#' The name of data file should follow the following naming convention: 
-#' \code{prefix} and a \code{suffix} seperated by an underscore. 
+#' Convenience function for loading data.
+#'
+#' The function recusively looks for the desired file in
+#' \code{"./input"}.
+#' The name of data file should follow the following naming convention:
+#' \code{prefix} and a \code{suffix} seperated by an underscore.
 #' Use dashes to seperate terms in the \code{prefix} and \code{suffix}.
 #' For example, \code{"train-stage1_tfidf-sublinear.csv"}.
 #'
-#' @param prefix Either the full name of the file to load (e.g., 
+#' @param prefix Either the full name of the file to load (e.g.,
 #' \code{"train_features.csv"} or the prefix of the file to load.  If the full
-#' name is used then the \code{suffix} parameter should be \code{NULL} and the 
+#' name is used then the \code{suffix} parameter should be \code{NULL} and the
 #' file extension should be included.
-#' 
+#'
 #' @param suffix Suffix of the data file to load. This parameter could be useful
 #' when interating through features; e.g., in a for loop. (default is \code{NULL})
-#' 
-#' @param file_ext Determines the file extension of the file to load. 
-#' \code{loadData()} calls the appropriate function to load the data. 
-#' Accepted values are \code{"csv", "rds", "rda", "xgb"}.  If 
-#' \code{file_ext = "xgb"} then the file to load is expected to be an 
+#'
+#' @param file_ext Determines the file extension of the file to load.
+#' \code{loadData()} calls the appropriate function to load the data.
+#' Accepted values are \code{"csv", "rds", "rda", "xgb"}.  If
+#' \code{file_ext = "xgb"} then the file to load is expected to be an
 #' xgb model.  (default is \code{"csv"})
-#' 
-#' @param verbose Whether or not to print details about the file being loaded. 
+#'
+#' @param verbose Whether or not to print details about the file being loaded.
 #' (default is \code{TRUE})
-#' 
+#'
 #' @param ... Additional parameters to pass the load function.
-#' 
-#' @seealso \code{\link{saveData}} The complimentary function to \code{loadData}.  Data 
-#' is saved with a naming convention compatible with \code{loadData}.  
-#' 
+#'
+#' @seealso \code{\link{saveData}} The complimentary function to \code{loadData}.  Data
+#' is saved with a naming convention compatible with \code{loadData}.
+#'
 #' @examples
 #' # load "train_features.csv"
-#' loadData("train_features") 
-#' 
+#' loadData("train_features")
+#'
 #' # loads "train_features.csv"
-#' loadData("train", suffix="features") 
-#' 
+#' loadData("train", suffix="features")
+#'
 #' # loads "train_features.rds"
-#' loadData("train", suffix="features", file_ext="rds") 
-#' 
+#' loadData("train", suffix="features", file_ext="rds")
+#'
 #' @keywords load
-#' 
+#'
 #' @return The desired data to be loaded.
-loadData <- function(prefix, suffix=NULL, file_ext="csv", verbose=TRUE,...) {
-  DATA_PATH <- "data"
-  
+loadData <- function(prefix, suffix=NULL, file_ext="csv", verbose=TRUE, data_path='./input',...) {
+
+
   # parse NULL inputs
   parseNULL <- function(prefix, x) ifelse(is.null(x), "", paste0(prefix, x))
-  file_name <- sprintf("%s%s.%s", prefix, parseNULL("_", suffix), file_ext)    
-  
+  file_name <- sprintf("%s%s.%s", prefix, parseNULL("_", suffix), file_ext)
+
   # lookup file
-  all_files <- dir(DATA_PATH, recursive=T, full.names=T)
+  all_files <- dir(data_path, recursive=T, full.names=T)
   full_name <- all_files[grepl(paste0("/", file_name), all_files)]
-  
+
   # check if file can be loaded
-  if (length(full_name)>=2L) 
+  if (length(full_name)>=2L)
     stop(sprintf("Multiple files found for %s:\n%s",
                  file_name, paste(full_name, collapse="\n")))
-  
-  if (length(full_name)==0L) 
+
+  if (length(full_name)==0L)
     stop(sprintf("%s not found", file_name))
-  
+
   # load file
   if (verbose) cat("   Loading", file_name, "\n")
   switch(
@@ -72,5 +72,5 @@ loadData <- function(prefix, suffix=NULL, file_ext="csv", verbose=TRUE,...) {
     xgb = xgb.load,
     json = fromJSON,
     stop(sprintf("Could not load %s", file_name))
-  )(full_name, ...)     
+  )(full_name, ...)
 }
--- R/saveData.R
+++ R/saveData.R
@@ -1,83 +1,80 @@
-#' Convenience function for saving data. 
-#' 
-#' The function saves the desired data in the 
-#' \code{"C:/Dropbox/Kaggle/Instacart/data"} directory.  
-#' The name of data file will adhere the following naming convention: 
-#' \code{prefix} and a \code{suffix} seperated by an underscore. 
+#' Convenience function for saving data.
+#'
+#' The function saves the desired data in the \code{"./input/*"} directory.
+#' The name of data file will adhere the following naming convention:
+#' \code{prefix} and a \code{suffix} seperated by an underscore.
 #' Use dashes to seperate terms in the \code{prefix} and \code{suffix}.
 #' For example, \code{"train-stage1_tfidf-sublinear.csv"}.
 #'
-#' @param prefix Either the full name of the file to save (e.g., 
+#' @param prefix Either the full name of the file to save (e.g.,
 #' \code{"train_features.csv"} or the prefix of the file to save.  If the full
-#' name is used then the \code{suffix} parameter should be \code{NULL} and the 
+#' name is used then the \code{suffix} parameter should be \code{NULL} and the
 #' file extension should be included.
-#' 
+#'
 #' @param suffix Suffix of the data file to save This parameter could be useful
 #' when interating through features; e.g., in a for loop. (default is \code{NULL})
-#' 
-#' @param folder Folder within \code{"C:/Dropbox/Kaggle/Mercedes/Data"} to save
+#'
+#' @param folder Folder within \code{"./input/"} to save
 #' the file in. (default is \code{NULL})
-#' 
-#' @param subfolder Subfolder with in \code{folder} to save the file in. 
+#'
+#' @param subfolder Subfolder with in \code{folder} to save the file in.
 #' (default is \code{NULL})
-#' 
-#' @param file_ext Determines the file extension of the file to save 
-#' \code{saveData()} calls the appropriate function to save the data. 
-#' Accepted values are \code{"csv", "rds", "rda", "xgb"}.  If 
-#' \code{file_ext = "xgb"} then the file to save is expected to be an 
+#'
+#' @param file_ext Determines the file extension of the file to save
+#' \code{saveData()} calls the appropriate function to save the data.
+#' Accepted values are \code{"csv", "rds", "rda", "xgb"}.  If
+#' \code{file_ext = "xgb"} then the file to save is expected to be an
 #' xgb model.  (default is \code{"csv"})
-#' 
+#'
 #' @param overwrite Whether or not an existing file with the same name should be
 #' overwritten. If \code{overwrite=NA} then the user is asked whether the file
 #' should be overwritten.  (default is \code{TRUE})
-#' 
-#' @param verbose Whether or not to print details about the file being saved 
+#'
+#' @param verbose Whether or not to print details about the file being saved
 #' (default is \code{TRUE})
-#' 
+#'
 #' @param ... Additional parameters to pass the save function.
-#' 
+#'
 #' @seealso \code{\link{loadData}} The complimentary function to \code{saveData}.
-#' 
+#'
 #' @examples
 #' # save "train_features.csv" in ./Data/clusters/skmeans
-#' saveData("train_features", folder="clusters", subfolder="skmeans") 
-#' 
+#' saveData("train_features", folder="clusters", subfolder="skmeans")
+#'
 #' # save "train_features.csv"
-#' saveData("train", suffix="features") 
-#' 
+#' saveData("train", suffix="features")
+#'
 #' # save "train_features.rds"
-#' saveData("train", suffix="features", file_ext="rds") 
-#' 
+#' saveData("train", suffix="features", file_ext="rds")
+#'
 #' @keywords save
-#' 
+#'
 saveData <- function(dt, prefix, suffix=NULL, folder=NULL, subfolder=NULL,
-                     file_ext="csv", overwrite=TRUE, verbose=TRUE, ...) {
+                     file_ext="csv", overwrite=TRUE, verbose=TRUE, data_path='./input', ...) {
 
-  DATA_PATH <- "data"
-  
   getInput <- function() {
     cat("   The file", file_name, "already exists.\n")
     cat("   Do you want to overwrite it? (T/F)")
-    as.logical(readline(prompt="   "))    
+    as.logical(readline(prompt="   "))
   }
-  
+
   # create file name
   parseNULL <- function(prefix, x) ifelse(is.null(x), "", paste0(prefix, x))
-  file_name <- sprintf("%s%s.%s", prefix, parseNULL("_", suffix), file_ext)    
-  
+  file_name <- sprintf("%s%s.%s", prefix, parseNULL("_", suffix), file_ext)
+
     # add path to the file name
   folder    <- parseNULL("/", folder)
   subfolder <- parseNULL("/", subfolder)
-  full_path <- sprintf("%s%s%s/", DATA_PATH, folder, subfolder)
+  full_path <- sprintf("%s%s%s/", data_path, folder, subfolder)
   full_name <- paste0(full_path, file_name)
-  
+
   # try to save the file
   if (file.exists(full_name)) {
-    while (!is.logical(overwrite) || is.na(overwrite)) { overwrite <- getInput()} 
+    while (!is.logical(overwrite) || is.na(overwrite)) { overwrite <- getInput()}
   } else {overwrite <- TRUE}
-  
+
   if (overwrite) {
-    dir.create(full_path, showWarnings=F, recursive=T)    
+    dir.create(full_path, showWarnings=F, recursive=T)
     if (verbose) cat("   Saving", file_name, "\n")
     switch(
       file_ext,
